b766c7b876b9486e458617913abeede8cb84efbc
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-04-15 15:45:26 +0800

    sas

---
+++ scripts/cmds/advice.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const srod = require('srod-v2');
+
+module.exports = {
+  config: {
+    name: 'advice',
+    version: '1.0',
+    author: 'zach',
+    countDown: 5,
+    role: 0,
+    shortDescription: '',
+    longDescription: {
+      en: 'Get a random advice.',
+    },
+    category: 'study',
+    guide: {
+      en: '{prefix} <>',
+    },
+    langs: {
+        en: {
+          final: "🤖 | TRANSLATING |",
+          loading: "🤖 | TRANSLATING |\n━━━━━━━━━━━━━━━\n⏳ | 𝙋𝙡𝙚𝙖𝙨𝙚 𝙬𝙖𝙞𝙩......\n━━━━━━━━━━━━━━━\n",
+        },
+    }
+  },
+
+  onStart: async function ({ api, event, args, message, getLang }) {
+    try {
+        const loadingMessage = getLang("loading");
+      const loadingReply = await message.reply(loadingMessage);
+      const adviceResult = await srod.GetAdvice();
+      const advice = adviceResult.embed.description;
+
+      let translatedAdvice = await translateAdvice(advice);
+
+      let messageToSend = `𝙎𝙤𝙥𝙝𝙞𝙖 𝘼𝙄:  ${translatedAdvice}`;
+
+      return api.editMessage(messageToSend, loadingReply.messageID);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+
+async function translateAdvice(advice) {
+  try {
+    const response = await axios.get(
+      `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURIComponent(advice)}`
+    );
+    const translations = response.data[0];
+    const translatedAdvice = translations.reduce((accumulator, translation) => {
+      if (translation[0]) {
+        accumulator += translation[0];
+      }
+      return accumulator;
+    }, '');
+    return translatedAdvice;
+  } catch (error) {
+    console.error(error);
+    return 'Error translating advice.';
+  }
+}
