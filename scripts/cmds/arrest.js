19e2f29e613e3ef54f20f553bc01f832e1c482e6
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-04-15 18:37:02 +0800

    sa

--- scripts/cmds/arrest.js
+++ scripts/cmds/arrest.js
@@ -1,50 +1,31 @@
-const axios = require('axios');
-const jimp = require("jimp");
-const fs = require("fs")
-
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
 
 module.exports = {
- config: {
- name: "finger",
- aliases: ["fg"],
- version: "1.0",
- author: "Upen Basnet & remake by AceGun", //don't change credit or G'ban
- countDown: 5,
- role: 0,
- shortDescription: "@mentiom",
- longDescription: "",
- category: "fun",
- guide: "{pn}"
- },
+  config: {
+    name: "rip",
+    version: "1.1",
+    author: "MILAN",
+    countDown: 5,
+    role: 0,
+    shortDescription: "rip image",
+    longDescription: "rip image",
+    category: "fun",
+    guide: {
+      vi: "{pn} [@tag | blank]",
+      en: "{pn} [@tag]"
+    }
+  },
 
-
-
- onStart: async function ({ message, event, args }) {
- const mention = Object.keys(event.mentions);
- if (mention.length == 0) return message.reply("Please mention someone");
- else if (mention.length == 1) {
- const one = event.senderID, two = mention[0];
- bal(one, two).then(ptth => { message.reply({ body: "「 Ahh felt so good🥵❤️ 」", attachment: fs.createReadStream(ptth) }) })
- } else {
- const one = mention[1], two = mention[0];
- bal(one, two).then(ptth => { message.reply({ body: "「 Ahh felt so good🥵❤️ 」", attachment: fs.createReadStream(ptth) }) })
- }
- }
-
-
+  onStart: async function ({ event, message, usersData }) {
+ const uid = Object.keys(event.mentions)[0]
+ if(!uid) return message.reply("please mention someone")
+    const avatarURL = await usersData.getAvatarUrl(uid);
+    const img = await new DIG.Rip().getImage(avatarURL);
+ const pathSave = `${__dirname}/tmp/${uid}_Rip.png`;
+    fs.writeFileSync(pathSave, Buffer.from(img));
+    message.reply({
+      attachment: fs.createReadStream(pathSave)
+    }, () => fs.unlinkSync(pathSave));
+  }
 };
-
-async function bal(one, two) {
-
- let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
- avone.circle()
- let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
- avtwo.circle()
- let pth = "abcd.png"
- let img = await jimp.read("https://i.imgur.com/YamClTu.jpeg")
-
- img.resize(1080, 1350).composite(avone.resize(300, 300), 420, 80).composite(avtwo.resize(300, 300), 95, 210);
-
- await img.writeAsync(pth)
- return pth
-   }
