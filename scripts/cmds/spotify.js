92ab67d9a349bfb027e24c8a7ae7ed3129c1e35f
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2024-04-16 17:17:31 +0800

    sdada

--- scripts/cmds/spotify.js
+++ scripts/cmds/spotify.js
@@ -1,94 +1,104 @@
-const axios = require("axios");
-const fs = require('fs');
-const path = require('path');
-
-function formatSize(bytes) {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return '0 Byte';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
+const { get } = require("axios");
+const { getPrefix, getStreamFromUrl } = global.utils;
 
 module.exports = {
-  config: {
-    name: "spotify",
-    version: "1.0",
-    author: "Rishad",
-    countDown: 10,
-    role: 0,
-    shortDescription: "play songs from spotify",
-    longDescription: "Download Spotify musics by searching",
-    category: "music",
-    guide: "{pn} song title"
-  },
-  lang: {
-    en:  {
-      loading: "⏳loading......."
-    }
-  },
-  onStart: async function ({ api, event, args, message }) {
-    const query = args.join(" ");
+	config: {
+		name: "spotify",
+		version: "1.3",
+		author: "Jun Jaam",
+		countDown: 0,
+		role: 0,
+		shortDescription: "download Spotify",
+		longDescription: "",
+		category: "media",
+		guide: {
+			en: "{pn} title or Spotify link"
+		}
+	},
 
-    if (!query) {
-      return message.reply("provide song by title");
-    }
+	onStart: async function ({ message, args, event }) {
+		let q = args.join(" ");
+		if (!q) {
+			return message.reply("Please add a title or Spotify link.\nExample: " +
+				`${await getPrefix(event.threadID)}${this.config.name} <title> or <Spotify link>`);
+		}
 
-    const SearchapiUrl = `https://for-devs.onrender.com/api/spsearch?apikey=fuck&query=${encodeURIComponent(query)}`;
+		if (q.includes("spotify.com")) {
+			const ok = /^https:\/\/open\.spotify\.com\/track\/[a-zA-Z0-9]+\?[a-zA-Z0-9\-_=&]+$/i;
+			if (ok.test(q)) {
+				try {
+					const sptf = await get(`https://test-ai-ihc6.onrender.com/mid/spotify-v2?url=${q}`);
+					const { av, result } = sptf.data;
+					const dl = await message.reply("Downloading the track, please wait...");
+					message.reply({
+						body: result,
+						attachment: await getStreamFromUrl(av)
+					});
+					message.unsend(dl.messageID);
+				} catch (error) {
+					return message.reply(error);
+				}
+			} else {
+				return message.reply("Please enter a valid Spotify URL");
+			}
+		} else {
+			const emj = ["👍", "😮", "😆", "😢", "❤"];
+			try {
+				const res = await get(`https://api-samir.onrender.com/spotifysearch?q=${q}`);
+				const r = res.data.data.slice(0, 5);
+				let result = "";
+				r.forEach((d, i) => {
+					const { title, imageUrl, popularity, artist, genres, releaseDate, duration } = d;
+					result += `${i + 1}.${emj[i]} Title: ${title}\nPopularity: ${popularity}\nArtist: ${artist.join(", ")}\nRelease Date: ${releaseDate}\nGenre: ${genres.join(", ")}\nDuration: ${duration}\n\n`;
+				});
 
-    try {
-      const response = await axios.get(SearchapiUrl);
-      const tracks = response.data.slice(0, 6);
+				const spotimg = r.map(j => j.imageUrl);
+				const _ = await Promise.all(spotimg.map(async url => await getStreamFromUrl(url)));
+				const info = await message.reply({
+					body: "React only to download the track\n\n" + result,
+					attachment: _
+				});
 
-      if (tracks.length === 0) {
-        return message.reply("❎ No tracks found for the given query.");
-      }
+				const n = {
+					commandName: this.config.name,
+					author: event.senderID,
+					"👍": r[0].url,
+					"😮": r[1].url,
+					"😆": r[2].url,
+					"😢": r[3].url,
+					"❤": r[4].url,
+					spoti: false,
+					d: info
+				};
 
-      const topTrack = tracks[0]; // Selecting the top track
-      const loadingMessage = this.lang.en.loading;
+				global.GoatBot.onReaction.set(info.messageID, n);
+			} catch (error) {
+				return message.reply(error);
+			}
+		}
+	},
 
-      const loadingReply = await message.reply(loadingMessage);
+	onReaction: async function ({ message, event, Reaction }) {
+		const { author, spoti, d } = Reaction;
+		if (event.userID !== author || spoti) return;
 
-      const SpdlApiUrl = 'https://for-devs.onrender.com/api/spotifydl?apikey=fuck&url=' + encodeURIComponent(topTrack.url);
+		const e = event.reaction;
+		if (e in Reaction) {
+			message.unsend(d.messageID);
+			const bsk = await message.reply("Downloading, please wait");
+			const track = Reaction[e];
 
-      const apiResponse = await axios.get(SpdlApiUrl);
-
-      if (apiResponse.data.id) {
-        const {
-          artists,
-          title,
-          album,
-          releaseDate,
-          downloadUrl
-        } = apiResponse.data;
-
-        const audioResponse = await axios.get(downloadUrl, { responseType: 'arraybuffer' });
-
-        const cacheDir = path.join(__dirname, 'cmds', 'cache');
-        if (!fs.existsSync(cacheDir)) {
-          fs.mkdirSync(cacheDir, { recursive: true });
-        }
-
-        const audioFilePath = path.join(cacheDir, 'spotifyAudio.mp3');
-        fs.writeFileSync(audioFilePath, Buffer.from(audioResponse.data));
-
-        const fileSize = fs.statSync(audioFilePath).size;
-        const sizeFormatted = formatSize(fileSize);
-
-        const attachment = fs.createReadStream(audioFilePath);
-
-        const form = {
-          body: `🎧 | Playing: ${title}...`,
-          attachment: attachment
-        };
-
-        api.editMessage(form, loadingReply.messageID);
-
-      } else {
-        message.reply("Sorry, the Spotify content could not be downloaded.");
-      }
-    } catch (error) {
-      console.error(error);
-      api.sendMessage("Error: " + error, event.threadID);
-    }
-  },
+			try {
+				const sptf = await get(`https://test-ai-ihc6.onrender.com/mid/spotify-v2?url=${track}`);
+				const { av, result } = sptf.data;
+				message.reply({
+					body: result,
+					attachment: await getStreamFromUrl(av)
+				});
+				message.unsend(bsk.messageID);
+			} catch (error) {
+				return message.reply(error);
+			}
+		}
+	}
 };
